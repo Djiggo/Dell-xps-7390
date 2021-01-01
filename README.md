de0a23c7b742623d2b317ca9aa0622fd081e402e
lev <gmNrr12oPi>
2021-01-01 14:29:26 +0300

    first version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Dell-xps-7390
+
+
+This script installs the minimal environment for me.
+
+Tested in Ubuntu 20.10  and Debian 11
+
+Packages will be installed:
+* google-chrome
+* git
+* docker
+* magic mouse 2 driver
+* IntelliJ IDEA
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+cd /tmp || exit
+mkdir "dell_install"
+
+apt update
+echo 'Installing dependencies...'
+apt instal -y dkms
+
+
+echo "Installing Google Chrome..."
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+
+echo "Installing Magic mouse 2 driver ..."
+
+git clone "https://github.com/RicardoEPRodrigues/Linux-Magic-Trackpad-2-Driver.git"
+cd Linux-Magic-Trackpad-2-Driver || exit
+chmod u+x install.sh
+sudo ./install.sh
+
+echo "Installing IntelliJ IDEA..."
+wget "https://download.jetbrains.com/product?code=IIU&latest&distribution=linux" -O idea.tar.gz
+tar -zxf idea.tar.gz -C /opt
+/opt/idea-IU-*/bin/idea.sh
+
+echo "Installing other packages..."
+
+apt instal -y git docker
